b2b9fd174dd992181e3ff8a46a8d525f309dff0f
camsi4 <camsi4@siame18.siame.univ-tlse3.fr>
2021-12-10 12:59:49 +0100

    maj SD

--- SD/tp.py
+++ SD/tp.py
@@ -2,11 +2,16 @@ import socket
 import json
 import random
 import sys
+import logging as log
+
+#from SD.chord_base import BUFFER_SIZE
+
+BUFFER_SIZE = 2048
 
 IP_perso="192.168.0.18"
 port_perso=1883
 
-num_noeud=0
+num_noeud = 0
 table_hachage={}
 table_voisinage = [[0, "", 0], [0, "", 0]]
 
@@ -19,17 +24,17 @@ answer_val = 0
 
 
 def my_new():
+    print(num_noeud)
     sendMsg(True,"new", table_voisinage[0][1], table_voisinage[0][2], IP_perso, port_perso, num_noeud)
 
 def my_join(IP, port):
     reponse = False
-
+    global num_noeud
     while(reponse==False):
         num_noeud=random.randint(1, 65535)
         sendMsg(True,"join", IP, port, IP_perso, port_perso, num_noeud)
         # Attente de la confirmation ou non
-        payload=receive()
-
+        payload, ok=receive()
         
         if(payload['type']=="init" and payload['key']==num_noeud):
             print("OKKKK pour la clé ", num_noeud)
@@ -75,10 +80,8 @@ def calcTvData(key):
     return tv, data
 
 
-def sendMsg(my, type, IP, port, IP_source = "", port_source = 0, key = 0, data = {}, tv = [], keyResp = 0, val = 0, idUnique = 0, msgGet = 0, msgPut = 0, msgGest = 0, ok = 0):
-    # Creation socket
-    
-        
+def sendMsg(my, type, IP, port, IP_source = "", port_source = 0, key = 0, data = {}, tv = [], keyResp = 0, val = 0, idUniq = 0, msgGet = 0, msgPut = 0, msgGest = 0, ok = 0):
+       
     jsonFrame = { }
     jsonFrame['type'] = type
 
@@ -97,7 +100,7 @@ def sendMsg(my, type, IP, port, IP_source = "", port_source = 0, key = 0, data =
         jsonFrame['val'] = val
 
     if(type=="put" or type=="ack"):
-        jsonFrame['idUnique'] = idUnique
+        jsonFrame['idUniq'] = idUniq
 
     if(type=="ack"):
         jsonFrame['ok'] = ok
@@ -111,55 +114,27 @@ def sendMsg(my, type, IP, port, IP_source = "", port_source = 0, key = 0, data =
         jsonFrame['msgGest'] = msgGest
 
     # envoi du message et fermeture de la socket
-    print("\n")
-    print("Envoi de ", type)
-    print(IP)
-    print(port)
+    print("Envoi de ", jsonFrame)
+    print("vers ", IP, " ", port, "\n")
     tpsoc_envoi = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #co = False
-    tpsoc_envoi.connect((IP, port))
-    #while(co==False):
-        #try:
-            #tpsoc_envoi.connect((IP, port))
-            #co=True
-        #except:
-            #print("fuck")
+    try:
+        tpsoc_envoi.connect((IP, port))
+    except:
+        try:
+            tpsoc_envoi.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            tpsoc_envoi.connect((IP, port))
+        except socket.error as e:
+            tpsoc_envoi.close()
+            print("ERREUR SEND")
+            return    
     tpsoc_envoi.send(bytes(json.dumps(jsonFrame), "utf-8"))
     tpsoc_envoi.close()
 
 
-def receive():
-    tpsoc_rcv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        tpsoc_rcv.bind(('', port_perso))
-    except socket.error as e:
-        print("ERREUR COUTE SUR LE PORT DE BASE")
-    tpsoc_rcv.listen(2)
-    try:
-        client, addr = tpsoc_rcv.accept()
-    except socket.error : 
-        print("ERREUR ACCEPT")
-    rec = ''
-    allReceived = False
-    try:
-        while(not allReceived):
-            incomingData = client.recv(2048).decode()
-            if(incomingData == ''):
-                allReceived = True
-            else:
-                rec += incomingData
-    except socket.error as e:
-        print("ERREUR RECEPTION DONNEES")
-        tpsoc_rcv.close()
-        return
-    payload = json.loads(rec)
-    #tpsoc_rcv.close()
-    return payload
-
-
 def decide(payload):
+    global num_noeud
     # OK
-    if(payload['type'] == "join"):
+    if(payload["type"] == "join"):
         key = payload['key']
         key_prec = table_voisinage[0][0]
         key_suiv = table_voisinage[1][0]      
@@ -180,7 +155,7 @@ def decide(payload):
                 sendMsg(False, "join", table_voisinage[0][1], table_voisinage[0][2], payload['ip'], payload['port'], payload['key'])
 
     # OK
-    elif(payload['type'] == "get_resp"):
+    elif(payload["type"] == "get_resp"):
         key = payload['key']
         key_prec = table_voisinage[0][0]
         key_suiv = table_voisinage[1][0]   
@@ -201,13 +176,13 @@ def decide(payload):
             sendMsg(False, "resp", payload['ip'], payload['port'], table_voisinage[1][1], table_voisinage[1][2], payload['key'], {}, [], table_voisinage[1][0])
 
     # OK
-    elif(payload['type'] == "resp"):
+    elif(payload["type"] == "resp"):
         ip_resp = payload['ip']
         port_resp = payload['port']
         key_resp = payload['keyResp']
 
     # OK
-    elif(payload['type'] == "new"):
+    elif(payload["type"] == "new"):
         print("New : ", payload['key'], " ", payload['ip'], " ", payload['port'])
         if(payload['ip'] != IP_perso):
             sendMsg(False, "new", table_voisinage[0][1], table_voisinage[0][2], payload['ip'], payload['port'], payload['key'])
@@ -219,30 +194,30 @@ def decide(payload):
                     majTv(payload)
     
     # OK
-    elif(payload['type']== "put"):
-        key = payload['key']
+    elif(payload["type"]== "put"):
+        key = payload["key"]
         key_prec = table_voisinage[0][0] 
 
         if(key_prec<num_noeud):
             if(key>key_prec and key<=num_noeud):
                 majData(payload)
-                sendMsg(False, "ack", payload['ip'], payload['port'], "", 0, 0, {}, [], 0, 0, payload['idUnique'], 0, 0, 0, "ok")
+                sendMsg(False, "ack", payload['ip'], payload['port'], "", 0, 0, {}, [], 0, 0, payload['idUniq'], 0, 0, 0, "ok")
             else:
                 sendMsg(False, "put", table_voisinage[0][1], table_voisinage[0][2], payload['ip'], payload['port'], payload['key'], {}, [], 0, payload['val'], payload['idUnique'])
         elif(key_prec>num_noeud):
             if((key>key_prec and key<=65535) and (key<=num_noeud and key>=0)):
                 majData(payload)
-                sendMsg(False, "ack", payload['ip'], payload['port'], "", 0, 0, {}, [], 0, 0, payload['idUnique'], 0, 0, 0, "ok")
+                sendMsg(False, "ack", payload['ip'], payload['port'], "", 0, 0, {}, [], 0, 0, payload['idUniq'], 0, 0, 0, "ok")
             else:
                 sendMsg(False, "put", table_voisinage[0][1], table_voisinage[0][2], payload['ip'], payload['port'], payload['key'], {}, [], 0, payload['val'], payload['idUnique'])
 
     # OK
-    elif(payload['ack']):
-        print("La valeur du \'put\' ", payload['idUnique'], " a été acceptée")
+    elif(payload["type"]=="ack"):
+        print("La valeur du \'put\' ", payload["idUnique"], " a été acceptée")
 
     # OK
-    elif(payload['type']=="get"):
-        key = payload['key']
+    elif(payload["type"]=="get"):
+        key = payload["key"]
         key_prec = table_voisinage[0][0]
 
         if(key_prec<num_noeud):
@@ -259,15 +234,52 @@ def decide(payload):
                 sendMsg(False, "get", table_voisinage[0][1], table_voisinage[0][2], payload['ip'], payload['port'], payload['key'])
 
     # OK
-    elif(payload['type']=="answer"):
-        print("La valeur du noeud ", payload['key'], " est ",payload['val']) 
-        answer_key = payload['key']
-        answer_val = payload['val']   
+    elif(payload["type"]=="answer"):
+        print("La valeur du noeud ", payload["key"], " est ",payload["val"]) 
+        answer_key = payload["key"]
+        answer_val = payload["val"]   
 
-    elif(payload['type'] == "quit" and payload['msgGest'] == num_noeud):
+    elif(payload["type"] == "quit" and payload["msgGest"] == num_noeud):
         return True
     
     return False
+
+
+def receive():
+    tpsoc_rcv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        tpsoc_rcv.bind(('', port_perso))
+    except socket.error as e:
+        try:
+            tpsoc_rcv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            tpsoc_rcv.bind(('', port_perso))
+        except socket.error as e:
+            tpsoc_rcv.close()
+            print("ERREUR ECOUTE")
+            return None, 0  
+    tpsoc_rcv.listen(2)
+    try:
+        client, addr = tpsoc_rcv.accept()
+    except socket.error as e: 
+        print("ERREUR ACCEPT")       
+    rec = ''
+    allReceived = False
+    try:
+        while(not allReceived):
+            incomingData = client.recv(2048).decode()
+            if(incomingData == ''):
+                allReceived = True
+            else:
+                rec += incomingData
+    except socket.error as e:
+        print("ERREUR RECEPTION DONNEES")
+        tpsoc_rcv.close()
+        return None, 0 
+    payload = json.loads(rec)
+    if(payload["type"]!=None):
+        tpsoc_rcv.close()
+        return payload, 1
+
 
 
 ##########################################################################################################################
@@ -294,8 +306,10 @@ else:
     print("ERROR lors du passage d'arguments!!!\n")
     stop = True
 
-'''
+
 while(stop == False):
-    payload = receive()
-    stop = decide(payload)
-'''
+    ok=0
+    payload, ok = receive()
+    if(ok==1):
+        decide(payload)
+        print(table_voisinage)
